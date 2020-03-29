2a4bf600fa5de28a35700be32c46548e3c13626c
Vadim Korobka <34455@mail.ua>
2020-03-29 22:10:32 +0300

    [Fix] Should in web env

--- src/helpers/validateAction.js
+++ src/helpers/validateAction.js
@@ -1,12 +1,8 @@
 import { isFSA } from 'flux-standard-action'
-import debug from 'debug'
-
-const log = debug('electron-redux:validateAction')
 
 export default function validateAction(action) {
   if (!isFSA(action)) {
-    log('WARNING! Action not FSA-compliant', action)
-
+    console.error('WARNING! Action not FSA-compliant', action)
     return false
   }
 
--- src/middleware/triggerAlias.js
+++ src/middleware/triggerAlias.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
-import assert from 'assert'
+// import assert from 'assert'
 import { ALIASED } from '../actions/alias'
 import aliasRegistry from '../registry/alias'
 
 const triggerAlias = store => next => action => {
   // TODO: store.dispatch() instead to not skip any middleware
   if (action.type === ALIASED) {
-    assert(action.meta && action.meta.trigger, 'No trigger defined')
+    // assert(action.meta && action.meta.trigger, 'No trigger defined')
     const alias = aliasRegistry.get(action.meta.trigger)
-    assert(alias, `Trigger alias ${action.meta.trigger} not found`)
+    // assert(alias, `Trigger alias ${action.meta.trigger} not found`)
     const args = action.payload || []
 
     // trigger alias
